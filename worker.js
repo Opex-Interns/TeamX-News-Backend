f011b4d47bf48662c0b8f801e953b3696b94c9d1
Bankole Ifeoluwa <ifeoluwa.bankole05@gmail.com>
2025-09-11 17:01:23 +0100

    Uncommented the worker.js

--- worker.js
+++ worker.js
@@ -1,14 +1,14 @@
-// // worker.js
-// import dotenv from "dotenv";
-// import cron from "node-cron";
-// import connectDB from "./config/db.js";
-// import { runNewsletterJob } from "./services/newsletterJob.js";
+// worker.js
+import dotenv from "dotenv";
+import cron from "node-cron";
+import connectDB from "./config/db.js";
+import { runNewsletterJob } from "./services/newsletterJob.js";
 
-// dotenv.config();
-// await connectDB();
+dotenv.config();
+await connectDB();
 
-// // Cron: run daily at 8:00 AM Africa/Lagos
-// cron.schedule("* * * * *", async () => {
-//   console.log("⏰ Running daily newsletter job…");
-//   await runNewsletterJob();
-// }, { timezone: process.env.TZ || "Africa/Lagos" });
+// Cron: run daily at 8:00 AM Africa/Lagos
+cron.schedule("* * * * *", async () => {
+  console.log("⏰ Running daily newsletter job…");
+  await runNewsletterJob();
+}, { timezone: process.env.TZ || "Africa/Lagos" });
